4aaa17e981f6d6667ad1e57dddab7640b5887838
niranjanimalla8288 <67537267+niranjanimalla8288@users.noreply.github.com>
2023-12-17 13:23:24 +0530

    Update Local_initExport_mahanidhi_1.sql
    test

--- Local_initExport_mahanidhi_1.sql
+++ Local_initExport_mahanidhi_1.sql
@@ -1,5 +1,7 @@
 use mahanidhi_db;
 
+' database changed
+
 DROP TABLE IF EXISTS `dbc_amenities`;
 /*!40101 SET @saved_cs_client = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
